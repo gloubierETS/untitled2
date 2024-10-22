87aba00a7708dbe05fe870f9f332fe196fbff0b3
RichieBits <louissimon30@gmail.com>
2024-10-22 09:55:53 -0400

    calcula

--- src/test.java
+++ src/test.java
@@ -4,8 +4,8 @@ public class test {
         System.out.println("salut mon bozo");
         System.out.println("C'est toi le bozo");
 
-        int x=1,y=3,z=0;
-        z= x+y;
+        int x=1,y=3,z;
+        z = x+y;
         System.out.println(z);
     }
 
