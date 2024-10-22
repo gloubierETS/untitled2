30e747a1c7b5c14f7e4ee1ab0069b2c01d3ed7a4
RichieBits <louissimon30@gmail.com>
2024-10-22 09:54:22 -0400

    calcul

--- src/test.java
+++ src/test.java
@@ -3,6 +3,10 @@ public class test {
 
         System.out.println("salut mon bozo");
         System.out.println("C'est toi le bozo");
+
+        int x=1,y=3,z=0;
+        z= x+y;
+        System.out.println(z);
     }
 
 }
